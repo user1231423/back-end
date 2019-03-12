81dd98633f61dee911b5eb43fec65781bd1dd9d7
João Gouveia <a13734@ebsaas.com>
2019-03-12 17:22:13 +0000

    Complete ex3

--- Ficha1/ex3.js
+++ Ficha1/ex3.js
@@ -1,5 +1,24 @@
-function operacao(num1,num2){
+function mult(num1,num2){
     return console.log(num1 * num2);
 }
 
-operacao(2,2)
+function som(num1,num2){
+    return console.log(num1 + num2);
+}
+
+function sub(num1,num2){
+    return console.log(num1 - num2);
+}
+
+function div(num1,num2){
+    return console.log(num1 - num2);
+}
+
+function elev(num1,num2){
+    return console.log(num1 - num2);
+}
+mult(2,2);
+som(2,2);
+sub(2,2);
+div(2,2)
+elev(2,2);
