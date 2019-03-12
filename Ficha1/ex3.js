cf03a8498c39181bc07af20974b7360111b22563
João Gouveia <a13734@ebsaas.com>
2019-03-12 17:27:32 +0000

    Change some values ex3

--- Ficha1/ex3.js
+++ Ficha1/ex3.js
@@ -11,14 +11,14 @@ function sub(num1,num2){
 }
 
 function div(num1,num2){
-    return console.log(num1 - num2);
+    return console.log(num1 / num2);
 }
 
 function elev(num1,num2){
-    return console.log(num1 - num2);
+    return console.log(Math.pow(num1,num2));
 }
 mult(2,2);
 som(2,2);
 sub(2,2);
-div(2,2)
-elev(2,2);
+div(3,1)
+elev(2,4);
