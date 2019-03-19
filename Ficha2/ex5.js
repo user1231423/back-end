1fd47a2b246f2d8c047219df6c62df8517b682bd
João Gouveia <a13734@ebsaas.com>
2019-03-19 21:16:05 +0000

    Work on ex5

--- Ficha2/ex5.js
+++ Ficha2/ex5.js
@@ -1,12 +1,36 @@
 function trabalho(entrada,saida){
-    if (entrada < 8 || entrada > 18 || saida > 18 || saida < 8 || entrada > saida){
-        return "Erro";
+    var horas_entrada = entrada.split(':');
+    var horas_saida = saida.split(':');
+
+    var enter = new Date();
+    enter.setHours(horas_entrada[0]);
+    enter.setMinutes(horas_entrada[1]);
+    enter.setSeconds(horas_entrada[2]);
+
+    var exit = new Date();
+    exit.setHours(horas_saida[0]);
+    exit.setMinutes(horas_saida[1]);
+    exit.setSeconds(horas_saida[2]);
+
+    var retornar = new Date();
+    var horas_trabalhadas = exit.getHours() - enter.getHours();
+    retornar.setHours(horas_trabalhadas);
+
+    var minutosTrabalho = exit.getMinutes() + enter.getMinutes();
+    if (minutosTrabalho <= 59){
+        retornar.setMinutes(minutosTrabalho);
+    }else if (minutosTrabalho == 60){
+        retornar.setHours(horas_trabalhadas + 1);
     }else{
-        var calculo = 0;
-        calculo = saida - entrada;
-        return calculo;
+        var min = minutosTrabalho / 60;
+        var string_min = min.toString();
+        var string_min_splited = string_min.split('.');
+        console.log(string_min_splited);
+        retornar.setHours(retornar.getHours() + 1);
+
     }
+
 }
 
 
-console.log(trabalho(9,18));
+trabalho('9:45:00','18:45:00');
