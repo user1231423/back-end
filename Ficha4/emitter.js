9ff3afce402253bb0e8467906de5a73333eedfda
João Gouveia <a13734@ebsaas.com>
2019-04-09 17:25:45 +0100

    Small change

--- Ficha4/emitter.js
+++ Ficha4/emitter.js
@@ -12,7 +12,7 @@ Emitter.prototype.on = function(type,listener){
 
 Emitter.prototype.emit = function(type){
     if(this.events[type] == undefined){
-        return "Utilize on primeiro!"; //Id does not exist return error
+        return "Utilize o on primeiro!"; //Id does not exist return error
     }else{
         this.events[type].forEach(function(element){ //Call each element inside the current type
             return element();
