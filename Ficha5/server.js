1e04467da520ecdbde2bbdfdd92407c914606d0c
João Gouveia <a13734@ebsaas.com>
2019-04-17 19:20:32 +0100

    Post and delete requests

--- Ficha5/server.js
+++ Ficha5/server.js
@@ -8,16 +8,65 @@ function readFile(fileName){
     return file;
 }
 
+function getIndex(obj,id,size){
+    var index;
+    for (var i = 0; i <= size; i++){
+        if (obj.persons[i].id == id){
+            index = i;
+        }
+    }
+    return index;
+}
+
+//Home page
 app.get('/', function (req, res){
     res.send('Hello World!');
 });
 
+//See all users
 app.get('/users', function(req,res){
     res.send(readFile('./persons.json'));
 });
 
-app.post('/user', function (req, res) {
-    res.send('entrou');
+//Add new user
+app.post('/new', function (req, res) {
+    var file = readFile('./persons.json');
+    var obj = JSON.parse(file); //Json to js Object
+    var size = Object.keys(obj).length; //Get the size of the object
+    var newId = obj.persons[size].id + 1; //New id = last person id + 1
+    //Create new person
+    var newPerson = {
+        id: newId,
+        firstName: "New",
+        lastName: "user",
+        gender: "M",
+        age: 30,
+        profession: "Unknown" 
+    }
+/*  var strNewPerson = JSON.stringify(newPerson);
+    strObj.concat(strNewPerson);
+    fs.appendFile('./persons.json',strNewPerson, (err) => {  
+        if (err) throw err;
+        console.log('The lyrics were updated!');
+    }); */
+    obj.persons.push(newPerson); //Push new person into persons array
+    var strObj = JSON.stringify(obj); //js object to Json
+    fs.writeFileSync('./persons.json',strObj); //Write json file
+    res.send('New user added'); //Return message new user added
+});
+
+app.delete('/user/:id', function (req, res) {
+    var id = req.params.id
+    var file = readFile('./persons.json');
+    var obj = JSON.parse(file); //Json to js Object
+
+    var size = obj.persons.length - 1;
+    var index = getIndex(obj,id,size);
+    obj.persons.splice(index,1); //Apagar o index recebido acima
+
+    var strObj = JSON.stringify(obj);
+    fs.writeFileSync('./persons.json',strObj); //Write json file
+    res.send('User deleted');
   });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
