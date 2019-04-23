1da694227ce588e5a778ede0b7c56e40013cac26
João Gouveia <a13734@ebsaas.com>
2019-04-23 18:04:24 +0100

    Add user from body data

--- Ficha5/server.js
+++ Ficha5/server.js
@@ -31,27 +31,41 @@ app.get('/users', function(req,res){
     res.send(readFile('./persons.json'));
 });
 
-//Add new user
+//Add new user from body data
 app.post('/new', function (req, res) {
     var file = readFile('./persons.json');
     var obj = JSON.parse(file); //Json to js Object
     var size = obj.persons.length - 1; //Get the size of the object
     var newId = obj.persons[size].id + 1; //New id = last person id + 1
-    //Create new person
-    var newPerson = {
-        id: newId,
-        firstName: "New",
-        lastName: "user",
-        gender: "M",
-        age: 30,
-        profession: "Unknown" 
-    }
+    var newPerson = req.body; //Request body object
+    newPerson.id = newId; //Add id value to object
 /*  var strNewPerson = JSON.stringify(newPerson);
     strObj.concat(strNewPerson);
     fs.appendFile('./persons.json',strNewPerson, (err) => {  
         if (err) throw err;
         console.log('The lyrics were updated!');
     }); */
+    obj.persons.push(newPerson); //Push new person into persons array
+    var strObj = JSON.stringify(obj); //js object to Json
+    fs.writeFileSync('./persons.json',strObj); //Write json file
+    res.send('New user added'); //Return message new user added
+});
+
+//Add new user
+app.post('/new/simple', function (req, res) {
+    var file = readFile('./persons.json');
+    var obj = JSON.parse(file); //Json to js Object
+    var size = obj.persons.length - 1; //Get the size of the object
+    var newId = obj.persons[size].id + 1; //New id = last person id + 1
+    //Create new person
+    var newPerson = {
+        firstName: "New",
+        lastName: "user",
+        gender: "M",
+        age: 30,
+        profession: "Unknown",
+        id: newId
+    }
     obj.persons.push(newPerson); //Push new person into persons array
     var strObj = JSON.stringify(obj); //js object to Json
     fs.writeFileSync('./persons.json',strObj); //Write json file
