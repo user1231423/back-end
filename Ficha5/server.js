97da7b7cded779d262142f2fa457dc7c443e47cc
João Gouveia <a13734@ebsaas.com>
2019-04-23 17:51:08 +0100

    Add body request

--- Ficha5/server.js
+++ Ficha5/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 function readFile(fileName){
     var file = fs.readFileSync(fileName, 'utf-8');
@@ -56,9 +58,9 @@ app.post('/new', function (req, res) {
     res.send('New user added'); //Return message new user added
 });
 
-//Delete user from given id
+//Delete user from given id with params
 app.delete('/user/:id', function (req, res) {
-    var id = req.params.id
+    var id = req.params.id;
     var file = readFile('./persons.json');
     var obj = JSON.parse(file); //Json to js Object
 
@@ -71,9 +73,35 @@ app.delete('/user/:id', function (req, res) {
     res.send('User deleted');
 });
 
-//See user info from given id
+//Delete user from given id with body
+app.delete('/user', function (req, res) {
+    var id = req.body.id; //Get id from body
+    var file = readFile('./persons.json');
+    var obj = JSON.parse(file); //Json to js Object
+
+    var size = obj.persons.length - 1;
+    var index = getIndex(obj,id,size);
+    obj.persons.splice(index,1); //Apagar o index recebido acima
+
+    var strObj = JSON.stringify(obj);
+    fs.writeFileSync('./persons.json',strObj); //Write json file
+    res.send('User deleted');
+});
+
+//See user info from given id with params
 app.get('/user/:id', function(req,res){
     var id = req.params.id; //Get id from params
+    var file = readFile('./persons.json');
+    var obj = JSON.parse(file);
+
+    var size = obj.persons.length - 1;
+    var index = getIndex(obj,id,size); //Get index of the given id
+    res.json(obj.persons[index]); //Send json object as response
+});
+
+//See user info from given id with body
+app.get('/user', function(req,res){
+    var id = req.body.id; //Get id from body
     var file = readFile('./persons.json');
     var obj = JSON.parse(file);
 
