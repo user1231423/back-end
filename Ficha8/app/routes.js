0c052b25e63bb43e1acbbb43b3df017bccbc2b06
João Gouveia <a13734@ebsaas.com>
2019-05-21 21:12:23 +0100

    Still some improvements to do

--- Ficha8/app/routes.js
+++ Ficha8/app/routes.js
@@ -1,5 +1,4 @@
 module.exports = function(app, passport) {
-
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
@@ -12,22 +11,38 @@ module.exports = function(app, passport) {
     // =====================================
     // 1 - show the login form GET
     // render the page and pass in any flash data if it exists 
+    app.get('/login', function(req, res) {
+        res.render('login.ejs',{ message: req.flash('loginMessage') }); // load the index.ejs file
+    });
 
     // 2 - process the login form POST
     // redirect to the secure profile section
     // redirect back to the signup page if there is an error
     // allow flash messages 
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile', //Redirect to profile if successful
+        failureRedirect: '/login', //Redirect back to signup in case of error
+        failureFlash: true //Allow flash messages
+    }));
 
     // =====================================
     // SIGNUP ==============================
     // =====================================
     // 3 - show the signup form
     // render the page and pass in any flash data if it exists GET
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs',{ message: req.flash('signupMessage') } ); // load the index.ejs file
+    });
 
     // 4- process the signup form POST
     // redirect to the secure profile section
     // redirect back to the signup page if there is an error
     // allow flash messages
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/profile', //Redirect to profile if successful
+        failureRedirect: '/signup', //Redirect back to signup in case of error
+        failureFlash: true //Allow flash messages
+    }));
 
     // =====================================
     // PROFILE SECTION =====================
@@ -36,12 +51,21 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     // get the user out of session and pass to template
+    app.get('/profile',isLoggedIn,function(req, res) {
+        res.render('profile.ejs', {
+            user: req.user
+        });    // load the index.ejs file
+    });
 
     // =====================================
     // LOGOUT ==============================
     // =====================================
     // 6 - logout user GET
     // redirect to root
+    app.get('/logout',isLoggedIn,function(req, res) {
+        req.logout();    // load the index.ejs file
+        res.redirect('/');
+    });
 };
 
 // route middleware to make sure a user is logged in
--- Ficha8/config/passport.js
+++ Ficha8/config/passport.js
@@ -5,12 +5,12 @@ var LocalStrategy = require('passport-local').Strategy;
 
 // MySQL connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'backend'
-  });
-  
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'backend'
+});
+
 console.log("MySQL connection created at %s with database: %s", connection.config.host, connection.config.database);
 
 // expose this function to our app using module.exports
@@ -29,7 +29,15 @@ module.exports = function (passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function (id, done) {
-        // select from users where id =        
+        // select from users where id = 
+        var sql = "SELECT * FROM users WHERE id = " + id;
+        connection.query(sql, function (error, results, fields) {
+            if (error) {
+                throw error;
+            } else {
+                done(error, results[0]);
+            }
+        });
     });
 
     // =========================================================================
@@ -37,7 +45,6 @@ module.exports = function (passport) {
     // =========================================================================
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
-
     passport.use('local-signup', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
         usernameField: 'email',
@@ -46,20 +53,58 @@ module.exports = function (passport) {
     },
         function (req, email, password, done) {
             // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists            
+            // we are checking to see if the user trying to login already exists 
+            var sql = "SELECT * FROM users WHERE email = ?";
+            connection.query(sql, [email], function (error, results, fields) {
+                if (error) {
+                    throw error;
+                } else if (Object.keys(results).length == 0) { //IF = 0 means it didn't return anything so it does not exist so we will create that user ->
+                    var sql = "INSERT INTO users SET email = ?, password = ?";
+                    connection.query(sql, [email, password], function (error, results, fields) { //Execute sql query and add data into the table users
+                        if (error) {
+                            throw error;
+                        } else {
+                            var userId = results.insertId;
+                            var sql = "SELECT * FROM users WHERE id = " + userId;
+                            connection.query(sql, function (error, results, fields) {
+                                if (error) {
+                                    throw error;
+                                } else {
+                                    var user = results[0];
+                                }
+                                return done(null, user);
+                            });
+                        }
+                    });
+                } else {
+                    return done(null, false, { message: req.flash('signupMessage', 'Email já existe!') });
+                }
+            });
         }));
+
     // =========================================================================
     // LOCAL LOGIN =============================================================
     // =========================================================================
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
-
     passport.use('local-login', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
         usernameField: 'email',
         passwordField: 'password',
         passReqToCallback: true // allows us to pass back the entire request to the callback
     },
-        function (req, email, password, done) { // callback with email and password from our form                        
+        function (req, email, password, done) { // callback with email and password from our form
+            var sql = "SELECT *  FROM users WHERE email = ? AND password = ?";
+            connection.query(sql,[email,password], function (error, results, fields) {
+                if (error){
+                    throw error;
+                }else{
+                    if (Object.keys(results).length == 0){
+                        return done(null,false,{ message: req.flash('loginMessage','Email ou password errados!') })
+                    }else{
+                        return done(null,results[0]);
+                    }
+                }
+            });                          
         }));
 };
