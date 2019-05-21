5d05efcaa3bae39d9efe9a59719832d642af8027
João Gouveia <a13734@ebsaas.com>
2019-05-21 18:13:16 +0100

    Start Ficha8

---
+++ Ficha8/config/passport.js
@@ -0,0 +1,59 @@
+// config/passport.js
+var mysql = require('mysql')
+// load all the things we need
+var LocalStrategy = require('passport-local').Strategy;
+
+// MySQL connection
+
+console.log("MySQL connection created at %s with database: %s", connection.config.host, connection.config.database);
+
+// expose this function to our app using module.exports
+module.exports = function (passport) {
+
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function (id, done) {
+        // select from users where id =        
+    });
+
+    // =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+        function (req, email, password, done) {
+            // find a user whose email is the same as the forms email
+            // we are checking to see if the user trying to login already exists            
+        }));
+    // =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+        function (req, email, password, done) { // callback with email and password from our form                        
+        }));
+};
