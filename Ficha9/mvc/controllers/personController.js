6c74665eb69e56a683845c361c7ac07187fed8c8
Joao Gouveia <joaogouveia.tc24@gmail.com>
2019-05-28 22:38:43 +0100

    Add validation on create user

--- Ficha9/mvc/controllers/personController.js
+++ Ficha9/mvc/controllers/personController.js
@@ -7,46 +7,51 @@ var connection = mysql.createConnection({
     database: 'backend'
 });
 
-exports.author_detail = function (req, res, next) {
+exports.author_detail = function(req, res, next) {
     userId = req.params.id;
     var sql = "SELECT * FROM persons WHERE Id = ?";
-    connection.query(sql, [userId], function (error, results, fields) {
+    connection.query(sql, [userId], function(error, results, fields) {
         if (error) {
             throw error
-        } if (Object.keys(results).length == 0) {
+        }
+        if (Object.keys(results).length == 0) {
             res.send("Utilizador não existe");
         } else {
-            res.json(results);
+            res.json(results[0]);
         }
     })
 };
 
-exports.get_allPersons = function (req, res, next) {
-    connection.query('SELECT * FROM persons', function (error, results, fields) {
+exports.get_allPersons = function(req, res, next) {
+    connection.query('SELECT * FROM persons', function(error, results, fields) {
         if (error) {
             throw error
         } else {
-            res.json(results);
+            res.json(results[0]);
         }
     })
 };
 
-exports.create_person = function (req, res, next) {
+exports.create_person = function(req, res, next) {
     var dataFromBody = req.body;
-    var sql = "INSERT INTO persons SET ?";
-    connection.query(sql, [dataFromBody], function (error, results, fields) {
-        if (error) {
-            throw error
-        } else {
-            res.json(results.insertId);
-        }
-    })
+    if (dataFromBody.length == undefined) {
+        res.send("Body sem dados!");
+    } else {
+        var sql = "INSERT INTO persons SET ?";
+        connection.query(sql, [dataFromBody], function(error, results, fields) {
+            if (error) {
+                throw error
+            } else {
+                res.json(results.insertId);
+            }
+        })
+    }
 };
 
-exports.delete_person = function (req, res, next) {
+exports.delete_person = function(req, res, next) {
     userId = req.params.id;
     var sql = "DELETE FROM persons WHERE Id = ?";
-    connection.query(sql, [userId], function (error, results, fields) {
+    connection.query(sql, [userId], function(error, results, fields) {
         if (error) {
             throw error
         } else if (results.affectedRows == 0) {
@@ -57,17 +62,17 @@ exports.delete_person = function (req, res, next) {
     })
 };
 
-exports.get_person_by_age_profession = function (req, res, next) {
+exports.get_person_by_age_profession = function(req, res, next) {
     userAge = req.params.age;
     userProfession = req.params.profession;
     var sql = "SELECT * FROM persons WHERE  Age = ? AND Profession = ?";
-    connection.query(sql, [userAge, userProfession], function (error, results, fields) {
+    connection.query(sql, [userAge, userProfession], function(error, results, fields) {
         if (error) {
             throw error
         } else if (Object.keys(results).length == 0) {
             res.send("Utilizador não existe");
         } else {
-            res.json(results);
+            res.json(results[0]);
         }
     })
 };
